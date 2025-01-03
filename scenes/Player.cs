0ae78ce8fa4ff0615c93d3c67161b84e2e5b7525
TwixieGames <collinmact@gmail.com>
2025-01-02 21:20:16 -0800

    Added collision for te wall tiles

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -5,23 +5,24 @@ public partial class Player : CharacterBody2D
 { 
 	[Export] int speed = 200;
 	Vector2 velocity;
+    RayCast2D TileDetect;
 
 	// Get the input from the player
 	private void GetInput()
 	{
 		velocity = Input.GetVector("ui_left", "ui_right","ui_up","ui_down") * speed;
-		Velocity = velocity;
 	}
 
 	// Called on run
 	public override void _Ready()
 	{
+        TileDetect = GetNode<RayCast2D>("TileDetect");
 	}
 
 	// Called every frame.
 	public override void _Process(double delta)
 	{
 		GetInput();
-		MoveAndSlide();
+		MoveAndCollide(velocity * (float)delta);
 	}
 }

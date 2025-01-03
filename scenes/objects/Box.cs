d5eacad520e86e7a4a161d458ea117bac929aab0
TwixieGames <collinmact@gmail.com>
2025-01-03 01:55:00 -0800

    Added collision and the ability to move the box around the scene by pushing with player character.

---
+++ scenes/objects/Box.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Box : StaticBody2D
+{
+	Vector2 velocity;
+	float Maxspeed = 50;
+
+	// Allows player to interact with the box through the use of an area2d
+	public void PlayerEnter(Player p)
+	{
+		velocity += p.velocity;	
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	
+		velocity = velocity.MoveToward(Vector2.Zero, 10);
+		MoveAndCollide(velocity * (float)delta);
+	}
+}

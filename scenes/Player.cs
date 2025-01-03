4cc68173319acd4a602f2da2acc9298f0f31fa60
TwixieGames <collinmact@gmail.com>
2025-01-02 22:25:01 -0800

    Immplemmented player movement on ice

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -6,16 +6,30 @@ public partial class Player : CharacterBody2D
 	[Export] int speed = 200;
 	Vector2 velocity;
     RayCast2D TileDetect;
+    TileMapLayer tileMap;
+    bool slide = false;
+
+    public void Detected()
+    {   
+        if(tileMap.GetCellAtlasCoords((Vector2I)tileMap.LocalToMap(Position)) == new  Vector2I(0,2))
+        {
+            GD.Print("Ice Detected");
+            slide = true;
+        }
+        else slide = false;
+    }
 
 	// Get the input from the player
 	private void GetInput()
 	{
-		velocity = Input.GetVector("ui_left", "ui_right","ui_up","ui_down") * speed;
+        if(!slide)
+		    velocity = Input.GetVector("ui_left", "ui_right","ui_up","ui_down") * speed;
 	}
 
 	// Called on run
 	public override void _Ready()
 	{
+        tileMap = GetParent().GetNode<TileMapLayer>("tiles");
         TileDetect = GetNode<RayCast2D>("TileDetect");
 	}
 
@@ -23,6 +37,7 @@ public partial class Player : CharacterBody2D
 	public override void _Process(double delta)
 	{
 		GetInput();
+        Detected();
 		MoveAndCollide(velocity * (float)delta);
 	}
 }
